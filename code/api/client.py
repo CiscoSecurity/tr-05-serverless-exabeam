b10a50074a7be689a3b68ec60d50359f80d534db
oshynk <80475008+oshynk@users.noreply.github.com>
2021-09-03 10:15:33 +0300

    CCTRI-3047 - Create health endpoint (#2)
    * [CCTRI-3047] Create client, health and tests updates
    
    * [CCTRI-3047] Fix flake issues

---
+++ code/api/client.py
@@ -0,0 +1,54 @@
+from flask import current_app
+import requests
+from requests.exceptions import (
+    SSLError,
+    ConnectionError,
+    MissingSchema,
+    InvalidSchema,
+    InvalidURL
+)
+
+from api.errors import (
+    AuthorizationError,
+    ExabeamSSLError,
+    ExabeamConnectionError,
+    CriticalExabeamResponseError
+)
+
+
+INVALID_CREDENTIALS = 'wrong key'
+
+
+class ExabeamClient:
+    def __init__(self, key):
+        self._headers = {
+            'ExaAuthToken': key,
+            'User-Agent': current_app.config['USER_AGENT']
+        }
+
+    @property
+    def _url(self):
+        url = current_app.config['EXABEAM_API_ENDPOINT']
+        return url.format(host=current_app.config['HOST'])
+
+    def health(self):
+        return self._request(path='api/auth/check')
+
+    def _request(self, path, method='GET', body=None,
+                 params=None, data_extractor=lambda r: r.json()):
+        url = '/'.join([self._url, path])
+
+        try:
+            response = requests.request(method, url, json=body,
+                                        params=params, headers=self._headers)
+        except SSLError as error:
+            raise ExabeamSSLError(error)
+        except (ConnectionError, MissingSchema, InvalidSchema, InvalidURL):
+            raise ExabeamConnectionError(self._url)
+        except UnicodeEncodeError:
+            raise AuthorizationError(INVALID_CREDENTIALS)
+
+        if response.ok:
+            return data_extractor(response)
+
+        raise CriticalExabeamResponseError(response)
