a2b46f62b81cb1e28048a9ad07f45dc4a99c29a0
oshynk <80475008+oshynk@users.noreply.github.com>
2021-10-04 12:15:11 +0300

    CCTRI-3256 - Categories per day tile implementation (#13)
    * [CCTRI-3255] Activity types per day tile implementation
    
    * [CCTRI-3255] Updates after develop merge
    
    * [CCTRI-3255] Donut tile refactoring
    
    * [CCTRI-3255] Update unit test
    
    * [CCTRI-3255] Updates due to flake8
    
    * [CCTRI-3255] Consistent naming
    
    * [CCTRI-3256] Categories per day tile implementation

--- code/api/tiles/__init__.py
+++ code/api/tiles/__init__.py
@@ -2,3 +2,4 @@ from api.tiles.affected_ips import AffectedIPsTile  # noqa: F401
 from api.tiles.activity_types import ActivityTypesTile  # noqa: F401
 from api.tiles.logs_category import CategoriesTile  # noqa: F401
 from api.tiles.activity_per_day import ActivityTypePerDayTile  # noqa: F401
+from api.tiles.categories_per_day import CategoriesPerDayTile  # noqa: F401
